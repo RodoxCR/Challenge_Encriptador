a978af5b42cbcb92b6d4db23ddcf9a7c8d9c5307
RodoxCR <rodolfor89@gmail.com>
2024-02-29 09:54:55 -0600

    update housePC

--- app.js
+++ app.js
@@ -1,29 +1,21 @@
-let textarea = document.getElementById('output_textarea');
 
 // Función para mostrar u ocultar la imagen de fondo del textarea
 function hidenBackgroundImage() {
-    if ((textarea.value.trim() === '') || (textarea.value.trim() === 'Ningún mensaje de texto a sido encriptado o desencriptado')) {
+    let textarea = document.getElementById('output_textarea');
+
+    if (textarea.value.trim() === '') {
         textarea.style.backgroundImage = "url('img/textNotFound.png')"; // Muestra la imagen de fondo
     } else {
         textarea.style.backgroundImage = 'none'; // Oculta la imagen de fondo
     }
 }
-
-// Mostrar la imagen de fondo al cargar la página (si el textarea está vacío)
 hidenBackgroundImage();
-
-// Escuchar el evento input para mostrar u ocultar la imagen de fondo
-textarea.addEventListener('input', hidenBackgroundImage);
-
+document.getElementById('output_textarea').addEventListener('input', hidenBackgroundImage);
 
 function applyTextareaAnimation(textareaAnimation) {
     textareaAnimation.addEventListener('click', function () {
         textareaAnimation.classList.add('shake'); // Agrega la clase 'shake' al hacer clic en el textarea
-
-        // Remueve la clase 'shake' después de 0.5 segundos para que la animación se detenga y el textarea vuelva a su estado original
-        setTimeout(function () {
-            textareaAnimation.classList.remove('shake');
-        }, 500); // 500 milisegundos (0.5 segundos)
+        setTimeout(function () { textareaAnimation.classList.remove('shake'); }, 500); // 500 milisegundos (0.5 segundos)
     });
 }
 
@@ -31,5 +23,20 @@ let inputTexarea = document.getElementById('input_textarea');
 let outputTexarea = document.getElementById('output_textarea');
 applyTextareaAnimation(inputTexarea);
 applyTextareaAnimation(outputTexarea);
+
+function validateText() {
+    let texto = document.getElementById("input_textarea").value;
+    let regex = /^[a-z]+$/; // Expresión regular para letras minúsculas sin acento
+    if (!regex.test(texto)) {
+        alert("El mensaje debe contener letras minúsculas, sin caracteres especiales y sin acento.");
+        return;
+    }
+
+    // Si pasa la validación, aquí puedes ejecutar la lógica para encriptar o desencriptar el texto
+    // Por ejemplo:
+    // encriptarTexto(texto);
+    // desencriptarTexto(texto);
+}
+
 
 
