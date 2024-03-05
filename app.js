348d7c4f5915be828059b293bcf4683148d4bfb9
RodoxCR <rodolfor89@gmail.com>
2024-03-04 18:12:09 -0600

    update night 4-3-2024 change css

--- app.js
+++ app.js
@@ -31,17 +31,20 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('input-textarea').addEventListener('input', function () {
         let inputText = this.value;
         let regex = /^[a-z\s]*$/; // Expresión regular para letras minúsculas y espacios en blanco
-
+        
         if (!regex.test(inputText)) {
+            
             errorMessage.style.display = 'inline-flex';
-            document.getElementById('encriptar').setAttribute('disabled', 'true');
-            document.getElementById('desencriptar').setAttribute('disabled', 'true');
-
+            document.getElementById('encrypt-button').setAttribute('disabled', 'true');
+            document.getElementById('decrypt-button').setAttribute('disabled', 'true');
+            // Elimina la clase de animación en los botones deshabilitados
+            classList.remove('container-buttons button:hover');
+            classList.remove('container-buttons button:hover');
 
         } else {
             errorMessage.style.display = 'none';
-            document.getElementById('encriptar').removeAttribute('disabled');
-            document.getElementById('desencriptar').removeAttribute('disabled');
+            document.getElementById('encrypt-button').removeAttribute('disabled');
+            document.getElementById('decrypt-button').removeAttribute('disabled');
 
         }
     });
@@ -50,33 +53,33 @@ document.addEventListener('DOMContentLoaded', function () {
 function encryptText() {
     let text = document.getElementById('input-textarea').value;
     let textConverted = text
-    .replace(/e/gi, "enter")
-    .replace(/i/gi, "imes")
-    .replace(/a/gi, "ai")
-    .replace(/o/gi, "ober")
-    .replace(/u/gi, "ufat");
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
 
-    if(text.length != 0){
+    if (text.length != 0) {
         document.getElementById('output-textarea').value = textConverted;
     }
 
-  document.getElementById('output-textarea').addEventListener('input', hiddenBackgroundImage());
+    document.getElementById('output-textarea').addEventListener('input', hiddenBackgroundImage());
 }
 
 function decryptText() {
     let text = document.getElementById('input-textarea').value;
     let textConverted = text
-    .replace(/enter/gi,"e")
-    .replace(/imes/gi,"i")
-    .replace(/ai/gi,"a")
-    .replace(/ober/gi,"o")
-    .replace(/ufat/gi,"u");
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
 
-    if(text.length != 0){
+    if (text.length != 0) {
         document.getElementById('output-textarea').value = textConverted;
     }
 
-  document.getElementById('output-textarea').addEventListener('input', hiddenBackgroundImage());
+    document.getElementById('output-textarea').addEventListener('input', hiddenBackgroundImage());
 }
 
 
